8db36f2a30295a8c67c6c4d2fbf7715e8803e65a
kimjava911 <aibackend4th@gmail.com>
2025-09-30 15:23:29 +0900

    push

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express"); // npm install express
 const path = require("path");
 const dotenv = require("dotenv"); // 불러들이기
+const { GoogleGenAI } = require("@google/genai");
 
 dotenv.config(); // .env 안에 있는 GEMINI_API_KEY
 // process.env.GEMINI_API_KEY
@@ -28,26 +29,35 @@ app.post("/gemini", async (req, res) => {
   // localhost:3000/gemini POST -> 응답
   // 1. fetch.
   const model = "gemini-2.5-flash-lite";
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent`;
+  //   const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent`;
   //   const apiKey = "";
   const apiKey = process.env.GEMINI_API_KEY;
-  const headers = {
-    "x-goog-api-key": apiKey,
-    "Content-Type": "application/json",
-  };
-  const payload = {
-    // contents: [{ parts: [{ text: "오늘 저녁 메뉴 추천" }] }],
-    contents: [{ parts: [{ text }] }],
-  };
-  const response = await fetch(url, {
-    headers,
-    method: "POST",
-    body: JSON.stringify(payload),
-  });
+  //   const headers = {
+  //     "x-goog-api-key": apiKey,
+  //     "Content-Type": "application/json",
+  //   };
+  //   const payload = {
+  //     // contents: [{ parts: [{ text: "오늘 저녁 메뉴 추천" }] }],
+  //     contents: [{ parts: [{ text }] }],
+  //   };
+  //   const response = await fetch(url, {
+  //     headers,
+  //     method: "POST",
+  //     body: JSON.stringify(payload),
+  //   });
   // 2. 라이브러리. (sdk)
   //   return res.json({ result: "ok" });
   // 비슷해보인다? -> .candidates... -> 원하는 것만 선택해서 내보내거나...
-  return res.json(await response.json());
+  const ai = new GoogleGenAI({
+    apiKey,
+  }); // SDK.
+  //   return res.json(await response.json());
+  const result = await ai.models.generateContent({
+    model, // 속성명과 변수명이 같으면 한 번만 입력
+    contents: text, // dom을 통해서 전달받은 text.
+  });
+  //   return res.json(await response.json());
+  return res.json(result);
 });
 
 // listen -> server를 구동. -> localhost:3000
